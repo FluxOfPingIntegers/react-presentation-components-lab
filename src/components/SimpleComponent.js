8b86dbcfc40fac3cf1d9be550b27a0457a87ca73
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-10-08 13:46:36 -0400

    commit tree 159a5d3720b30c2b2e26cb27256c2098bb896dbc

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,31 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      mood: props.mood,
+    }
+  }
+
+  handleClick = () => {
+    let mood = this.state.mood === "happy" ? "sad" : "happy";
+    this.setState({
+      mood: mood,
+    })
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    )
+  } 
+
+}
+
+SimpleComponent.defaultProps = {
+  mood: "happy",
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={function() {return (this.style.backgroundColor = 'red')}} />
   </div>,
   document.getElementById('root')
 );
